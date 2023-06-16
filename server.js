4a14cddd2c5515b62f39a4d69819cb717d513166
ratnajeet patil <100475083+ratnajeet00@users.noreply.github.com>
2023-06-16 18:59:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,71 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const dataFilePath = './user-data.json';
+const dbPath = './database.db';
+const db = new sqlite3.Database(dbPath);
 
-// Check if the user data file exists, if not, create it with default admin credentials
-if (!fs.existsSync(dataFilePath)) {
-  const defaultUser = { username: 'admin', password: 'admin' };
-  const defaultUserList = [defaultUser];
-  fs.writeFileSync(dataFilePath, JSON.stringify(defaultUserList));
-}
+// Create the user table if it doesn't exist
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+  )`);
+
+  // Check if the admin user exists, if not, create it
+  db.get('SELECT * FROM users WHERE username = ?', 'admin', (err, row) => {
+    if (!row) {
+      const stmt = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+      stmt.run('admin', 'admin');
+      stmt.finalize();
+    }
+  });
+});
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Read the user data from the file
-  const data = fs.readFileSync(dataFilePath, 'utf8');
-  const userList = JSON.parse(data);
-
-  const user = userList.find(user => user.username === username && user.password === password);
-
-  if (user) {
-    // Successful login
-    res.status(200).json({ message: 'Login successful' });
-  } else {
-    // Invalid credentials
-    res.status(401).json({ message: 'Invalid username or password' });
-  }
+  db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
+    if (row) {
+      res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
+    }
+  });
 });
 
 app.post('/addUser', (req, res) => {
   const { username, password } = req.body;
 
-  if (username && password) {
-    // Read the user data from the file
-    const data = fs.readFileSync(dataFilePath, 'utf8');
-    const userList = JSON.parse(data);
-
-    // Check if the username already exists
-    const existingUser = userList.find(user => user.username === username);
-    if (existingUser) {
+  db.get('SELECT * FROM users WHERE username = ?', username, (err, row) => {
+    if (row) {
       res.status(400).json({ message: 'Username already exists' });
     } else {
-      // Add the new user to the user list
-      userList.push({ username, password });
-
-      // Save the updated user list to the file
-      fs.writeFileSync(dataFilePath, JSON.stringify(userList));
+      const stmt = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+      stmt.run(username, password);
+      stmt.finalize();
 
       res.status(200).json({ message: 'User created successfully' });
     }
-  } else {
-    // Invalid data
-    res.status(400).json({ message: 'Invalid username or password' });
-  }
+  });
 });
 
 app.get('/userList', (req, res) => {
-  // Read the user data from the file
-  const data = fs.readFileSync(dataFilePath, 'utf8');
-  const userList = JSON.parse(data);
-
-  res.json(userList);
+  db.all('SELECT * FROM users', (err, rows) => {
+    res.json(rows);
+  });
 });
 
 const PORT = 3000;
