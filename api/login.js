b9e0b4baa6b5268855cd32445948db927d5439d6
Arya Maddel <maddelarya@gmail.com>
2023-08-06 16:54:00 +0530

    formatting

--- api/login.js
+++ api/login.js
@@ -1,8 +1,8 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== "POST") {
     res.status(405).end(); // Method Not Allowed
     return;
   }
@@ -11,20 +11,20 @@ export default async function handler(req, res) {
 
   // Connect to the SQLite database
   const db = await open({
-    filename: './database.db',
+    filename: "./database.db",
     driver: sqlite3.Database,
   });
 
   // Check if the user exists
   const user = await db.get(
-    'SELECT * FROM users WHERE username = ? AND password = ?',
+    "SELECT * FROM users WHERE username = ? AND password = ?",
     [username, password]
   );
 
   if (user) {
-    res.status(200).json({ message: 'Login successful' });
+    res.status(200).json({ message: "Login successful" });
   } else {
-    res.status(401).json({ message: 'Invalid username or password' });
+    res.status(401).json({ message: "Invalid username or password" });
   }
 
   await db.close();
